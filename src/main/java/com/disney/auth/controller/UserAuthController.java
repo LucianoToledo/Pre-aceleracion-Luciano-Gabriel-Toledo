0f5a1b2d3d6c8c7b0cf9da3bee28f49a94a9686f
Luciano Toledo <toledolucianogabriel@gmail.com>
2022-07-26 02:51:08 -0300

    all clases created

---
+++ src/main/java/com/disney/auth/controller/UserAuthController.java
@@ -0,0 +1,59 @@
+package com.disney.auth.controller;
+
+import com.disney.auth.dto.AuthenticationRequest;
+import com.disney.auth.dto.AuthenticationResponse;
+import com.disney.auth.dto.UserDTO;
+import com.disney.auth.service.JwtUtils;
+import com.disney.auth.service.UserDetailsCustomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class UserAuthController {
+    private UserDetailsCustomService userDetailsService;
+    private AuthenticationManager authenticationManager;
+    private JwtUtils jwtTokenUtil;
+
+    @Autowired
+    public UserAuthController(
+            UserDetailsCustomService userDetailsServicer,
+            AuthenticationManager authenticationManager,
+            JwtUtils jwtTokenutil) {
+        this.userDetailsService = userDetailsService;
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthenticationResponse> singUp(@RequestBody UserDTO userRequest) throws Exception {
+        this.userDetailsService.save(userRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    //TODO: pasar la logica de este controlador a un servicio, los controladores no deberian tener logica por una cuestion de responsabilidad
+    @PostMapping("/singin")
+    public ResponseEntity<AuthenticationResponse> singIn(@RequestBody AuthenticationRequest authRequest) throws Exception {
+        UserDetails userDetails;
+        try {
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+            userDetails = (UserDetails) auth.getPrincipal();
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or pasword");
+        }
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+}
+
---
+++ src/main/java/com/disney/auth/dto/UserDTO.java
@@ -0,0 +1,10 @@
+package com.disney.auth.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+
+    private String username;
+    private String password;
+}
